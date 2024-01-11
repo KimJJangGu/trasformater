8305f2daad2d092f9168c98f5fb33ff7f4ba9bc6
Kim DoHyeon <dohyun51731@gmail.com>
2024-01-11 22:32:00 +0900

    단위 변환 앱(가로 스크롤 기능 구현)

--- lib/presenter/length/length_screen.dart
+++ lib/presenter/length/length_screen.dart
@@ -28,131 +28,19 @@ class _LengthScreenState extends State<LengthScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Orientation orientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('길이 변환'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                TextField(
-                  controller: _lengthScreenViewModel.lengthController,
-                  keyboardType: TextInputType.number,
-                  onChanged: (value) {
-                    setState(() {
-                      _lengthScreenViewModel.length = double.tryParse(value) ?? 0;
-                      _lengthScreenViewModel.convertLength();
-                    });
-                  },
-                  decoration: InputDecoration(
-                    suffixIcon: DropdownButtonHideUnderline(
-                      child: DropdownButton<String>(
-                        isExpanded: false,
-                        value: _lengthScreenViewModel.fromUnit,
-                        onChanged: (value) {
-                          setState(() {
-                            _lengthScreenViewModel.fromUnit = value!;
-                            _lengthScreenViewModel.convertLength();
-                          });
-                        },
-                        items: _lengthScreenViewModel.units.map<DropdownMenuItem<String>>((String unit) {
-                          return DropdownMenuItem<String>(
-                            value: unit,
-                            child: Text(
-                              unit,
-                              style: const TextStyle(fontSize: 20),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                    labelText: '길이 입력',
-                    hintText: '길이를 입력하세요',
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                  ),
-                  style: const TextStyle(fontSize: 20),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(0.0),
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [],
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(2.0),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 230,
-                  child: ListView(
-                    children: _lengthScreenViewModel.transResult.map((result) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(
-                            textAlign: TextAlign.end,
-                            overflow: TextOverflow.ellipsis,
-                            (result),
-                            style: const TextStyle(fontSize: 16),
-                            maxLines: 1,
-                          ),
-                          const Divider(height: 4, color: Colors.black12),
-                        ],
-                      );
-                    }).toList(),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      foregroundColor: Colors.black,
-                      elevation: 2,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    onPressed: () {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text(
-                          '입력 값 초기화',
-                          textAlign: TextAlign.center,
-                        ),
-                        backgroundColor: Colors.black26,
-                      ));
-                      _lengthScreenViewModel.resetValues();
-                      setState(() {
-
-                      });
-                    },
-                    child: const Text('초기화', style: TextStyle(fontSize: 16)),
-                  ),
-                ),
-              ],
+      body: orientation == Orientation.portrait
+          ? Column(
+              children: [_lengthScreenWidget(), Expanded(child: _lengthImageWidget())],
+            )
+          : ListView(
+              children: [_lengthScreenWidget(), _lengthImageWidget()],
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Image.asset(
-                'assets/body_back/ladder.png',
-                fit: BoxFit.contain,
-              ),
-            ),
-          ),
-        ],
-      ),
       floatingActionButton: FloatingActionButton(
         elevation: 4,
         backgroundColor: Colors.white,
@@ -200,6 +88,120 @@ class _LengthScreenState extends State<LengthScreen> {
           borderRadius: BorderRadius.circular(50),
         ),
         child: const Icon(Icons.autorenew_rounded),
+      ),
+    );
+  }
+
+  Widget _lengthImageWidget() {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Image.asset(
+        'assets/body_back/ladder.png',
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  Widget _lengthScreenWidget() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: _lengthScreenViewModel.lengthController,
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              setState(() {
+                _lengthScreenViewModel.length = double.tryParse(value) ?? 0;
+                _lengthScreenViewModel.convertLength();
+              });
+            },
+            decoration: InputDecoration(
+              suffixIcon: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  isExpanded: false,
+                  value: _lengthScreenViewModel.fromUnit,
+                  onChanged: (value) {
+                    setState(() {
+                      _lengthScreenViewModel.fromUnit = value!;
+                      _lengthScreenViewModel.convertLength();
+                    });
+                  },
+                  items: _lengthScreenViewModel.units.map<DropdownMenuItem<String>>((String unit) {
+                    return DropdownMenuItem<String>(
+                      value: unit,
+                      child: Text(
+                        unit,
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+              labelText: '길이 입력',
+              hintText: '길이를 입력하세요',
+              focusedBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+              ),
+            ),
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(0.0),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
+          const Padding(
+            padding: EdgeInsets.all(2.0),
+          ),
+          ..._lengthScreenViewModel.transResult.map((result) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  textAlign: TextAlign.end,
+                  overflow: TextOverflow.ellipsis,
+                  (result),
+                  style: const TextStyle(fontSize: 16),
+                  maxLines: 1,
+                ),
+                const Divider(height: 4, color: Colors.black12),
+              ],
+            );
+          }).toList(),
+          const Padding(
+              padding: EdgeInsets.all(2)),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text(
+                    '입력 값 초기화',
+                    textAlign: TextAlign.center,
+                  ),
+                  backgroundColor: Colors.black26,
+                ));
+                _lengthScreenViewModel.resetValues();
+                setState(() {});
+              },
+              child: const Text('초기화', style: TextStyle(fontSize: 16)),
+            ),
+          ),
+        ],
       ),
     );
   }
